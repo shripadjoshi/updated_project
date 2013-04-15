4f636e310dbc2f545944fc6493c35f8256937cfd
Shripad <shripad.joshi@synechron.com>
2013-04-15 18:03:20 +0530

    editor functionality updates

--- app/helpers/additional_claims_helper.rb
+++ app/helpers/additional_claims_helper.rb
@@ -1,6 +1,10 @@
 module AdditionalClaimsHelper
   def accessibility_conditions(claim)
-    ((current_user.has_role? :admin) || (claim.assignee_name.eql?(current_user.name)) || 
-        claim.research_status.eql?("Unassigned") || (current_user.has_role? :editor))
+    if (current_user.has_role?(:admin) || current_user.has_role?(:chemist))
+      ((current_user.has_role? :admin) || (claim.assignee_name.eql?(current_user.name)) ||
+        claim.research_status.eql?("Unassigned") )
+    else
+      (claim.editor_name.eql?(current_user.name) || claim.copy_status.eql?("Unassigned") )
+    end
   end
 end
